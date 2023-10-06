7f85debad7ad149a7ea6b930075db8e75493f52f
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-06 18:03:39 +0200

    Product DTO: adding tags

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -44,6 +44,12 @@ export class Product {
   @Column('text')
   gender: string;
 
+  @Column('text', {
+    array: true,
+    default: [],
+  })
+  tags: string[];
+
   @BeforeInsert()
   @BeforeUpdate()
   checkSlug() {
