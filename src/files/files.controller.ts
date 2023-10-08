fc187d2c607eeed9e7ed5df9e891c380cb512995
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-10-08 12:28:30 +0200

    Uploading files: Get image by url

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,19 +1,33 @@
 import {
   BadRequestException,
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { Response } from 'express';
 
 import { fileFilter, fileNamer } from './helpers';
 import { diskStorage } from 'multer';
 
 @Controller('files')
 export class FilesController {
-  constructor(private readonly filesService: FilesService) {}
+  constructor(private readonly _filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() res: Response,
+    @Param('imageName') imageName: string,
+  ) {
+    const path = this._filesService.getStaticProductImage(imageName);
+
+    res.sendFile(path);
+  }
 
   @Post('product')
   @UseInterceptors(
@@ -29,8 +43,10 @@ export class FilesController {
   uploadProductImage(@UploadedFile() file: Express.Multer.File) {
     if (!file) throw new BadRequestException('Make sure that file is an image');
 
+    const secureUrl = `${file.filename}`;
+
     return {
-      fileName: file.filename,
+      secureUrl,
     };
   }
 }
